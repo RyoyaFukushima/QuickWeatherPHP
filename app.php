734ac52c54e3c60bec7825ac07f4716adddf5d3a
RyoyaFukushima <101489172+RyoyaFukushima@users.noreply.github.com>
2022-05-25 13:42:38 +0900

    initial-commit;

---
+++ app.php
@@ -0,0 +1,79 @@
+<?php
+// 天気ごとの画像切り替えがエラー起きるので修正
+require 'openWeatherAPI.php';
+header('X-FRAME-OPTION:DENY'); // クリックジャッキング対策
+
+function h($str){
+    return htmlspecialchars($str, ENT_QUOTES, 'UTF-8');
+}
+
+/*$sunny = file_get_contents("img/sunny.png");
+$cloud = file_get_contents('img/cloud.png');
+$rain = file_get_contents('img/rain.png');
+$snow = file_get_contents('img/snow.png');
+// 画像指定をしないとデータ（文字列がそのまま)表示される
+header('Content-type: image/png');*/
+
+// 0:タイトル画面, 1:天気画面
+$pageflag = 0;
+
+if(!empty($_POST['location'])){
+    $pageflag = 1;
+}
+if(isset($_POST['back'])){
+    $pageflag = 0;
+}
+?>
+
+<?php if($pageflag === 0) :?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="common/simple.css">
+<head></head>
+<body>
+    <h2>現在の天気を簡易表示するアプリ</h2>   
+    <form method="POST" action="app.php">
+        地名
+        <input type="text" name="location">
+        <br>
+        <input type="submit" name="btn_submit"value="送信する。">
+    </form>   
+</body>
+<html>
+<?php endif; ?>
+
+<?php if($pageflag === 1) :?>
+<!DOCTYPE html>
+<meta charset="utf-8">
+<link rel="stylesheet" href="common/simple.css">
+<head></head>
+<body>
+    <h2>天気結果</h2>
+    <?php 
+    $info = weatherinfo($_POST['location']);
+    $message = makemessage($info, $_POST['location']);
+    //$weather = weatherStatus($info);
+    // 天気ごとに画像切り替え
+    /*switch($weather){
+        case 'Clear':
+            echo $sunny;
+        break;
+        case 'Clouds':
+            echo $cloud;
+        break;
+        case 'Rain':
+            echo $rain;
+        break;
+        case 'Snow':
+            echo $snow;
+        break;
+    }*/
+    echo h($message);
+    ?>
+    <br>
+    <form method="POST" action="app.php">
+        <input type="submit" name="back"value="戻る。">
+    </form>
+</body>
+<html>
+<?php endif; ?>
---
+++ openWeatherAPI.php
@@ -0,0 +1,57 @@
+<?php
+// OpenWeatherAPIを使って現在の仙台の天気を取得してみる
+// TODO:GitHubに上げる場合はAPIKEYを隠す
+// TODO:次はフォームで地名を入力して任意の位置を取れるようにする
+/**
+ * ファイル全体をまるごと読み込みたい→ file_get_contents()
+ * ファイルの中身をまるごと出力したい→ readfile()
+ * 行単位のテキストファイルを配列として読み込みたい→ file()
+ * ファイルをバイト単位で読み込みたい→ fopen()+fread()
+ * CSVを読み込みたい→ SplFileObjectクラス
+ * オブジェクト指向的に操作したい→ SplFileObjectクラス
+ * クラウドとかFTPとかにあるファイルを読み込みたい→ League\Flysystemライブラリ
+ * 
+ * 引用:https://qiita.com/tadsan/items/bbc23ee596d55159f044
+ */
+
+function weatherinfo($location){
+    // とりあえず8都市くらい日本語化
+    $enloc = match($location){
+        '仙台'   => 'Sendai',
+        '東京'   => 'Tokyo',
+        '大阪'   => 'Osaka',
+        '札幌'   => 'Sapporo',
+        '名古屋' => 'Nagoya',
+        '京都'   => 'Kyoto',
+        '横浜'   => 'Yokohama'
+    };
+    // openweatherのAPIキーは別ファイルから読み込む
+    $API_KEY = file_get_contents('apikey.txt');
+    $url = "https://api.openweathermap.org/data/2.5/weather?q=${enloc}&units=metric&APPID=$API_KEY";
+    $res = file_get_contents($url);
+    $res_json = json_decode($res, true);
+
+    return $res_json;
+}
+function weatherStatus($json){
+    $weather = $json['weather'][0]['main'];
+    return $weather;
+}
+function makemessage($json, $location){
+    // 天気部分をパースして取得
+    $weather = $json['weather'][0]['main'];
+    // 気温部分をパースして取得
+    $temp = $json['main']['temp'];
+    // 英語の結果を日本語に簡易的に変換
+    $jpweather = match($weather){
+        'Clear' => '晴れ',
+        'Clouds' => '曇り',
+        'Rain' => '雨',
+        'Snow' => '雪'
+    };
+    $message = "現在の${location}の天気は${jpweather}です。気温は${temp}度です。";
+
+    return $message;
+}
+
+?>
